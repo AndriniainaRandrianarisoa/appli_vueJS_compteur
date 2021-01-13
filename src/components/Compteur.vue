2148a9c3443e063e5d53eefcd0ec4a7ec0cdfae3
Ange - BC <andriniaina.randria@gmail.com>
2021-01-13 15:30:58 +0100

    first commit

---
+++ src/components/Compteur.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>Mon super Compteur</h1>
+        <p @click="increment" >Vous avez cliqué {{ total }} fois</p>
+        <p>le Total general depuis VueX est {{ totalGeneral }} </p>
+        <p>Le double du total est de {{ double }} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Compteur",
+    data() {
+        return {
+            total:0
+        }
+    }, 
+    computed: {
+        totalGeneral() {
+            return this.$store.state.totalFromVueX 
+        },
+        double() {
+            return this.$store.getters.doubleDuTotal
+        }
+    },
+
+    methods: {
+        increment() {
+            this.total+=1;
+            this.$store.commit("setTotalFromVueX" , this.totalGeneral += 1)
+        }
+    } 
+}
+</script>
+
+<style scoped></style>
+
---
+++ src/views/Nouveau.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Nouveau">
+      <p>CVoucou je suis la nouvelle page </p>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name : "",
+  components: {
+    
+  }
+  
+}
+</script>
+ 
